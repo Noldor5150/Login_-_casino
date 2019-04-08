b790119c1778c9dcedc5b07571cb09bf693b8c02
Noldor5150 <ruikis.paulius@gmail.com>
2019-04-08 13:53:11 +0300

    kuriam session klases

---
+++ core/classes/user/Sesion.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Core\User;
+
+class Session extends \Core\User\Abstracts\Session{
+    
+    
+    
+    public function __construct(\Core\Modules\User\Repository $repo) {
+        
+    }
+
+    public function getUser(): Abstracts\User {
+        
+    }
+
+    public function isLoggedIn() {
+        
+    }
+
+    public function login($email, $password): int {
+        
+    }
+
+    public function loginViaCookie() {
+        
+    }
+
+    public function logout() {
+        
+    }
+
+}
