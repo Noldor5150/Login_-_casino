865fb0cd3ab07b3c361761ead5518f41dff506dd
Noldor5150 <ruikis.paulius@gmail.com>
2019-04-09 11:19:02 +0300

    darom sesion f-jas 1

--- core/classes/user/Sesion.php
+++ core/classes/user/Sesion.php
@@ -7,6 +7,8 @@ class Session extends \Core\User\Abstracts\Session{
     
     
     public function __construct(\Core\Modules\User\Repository $repo) {
+        $this->repo = $repo;
+        return $this->is_logged_in = false;
         
     }
 
@@ -15,6 +17,7 @@ class Session extends \Core\User\Abstracts\Session{
     }
 
     public function isLoggedIn() {
+        return $this->is_logged_in;
         
     }
 
